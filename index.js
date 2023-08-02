bc3d14d80e0d084de60afe15ce5658af0f23f99c
Luisa Perez <luisa.perezw3@gmail.com>
2023-08-02 23:04:46 +0100

    adding dots

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const slider = document.querySelector(".slider");
 
 const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
-// const indicatorParents = document.querySelector(".controls ul");
+const indicatorParents = document.querySelector(".controls ul");
 
 var sectionIndex = 0;
 
 function setIndex() {
-  // document.querySelector(".controls .selected").classList.remove("selected");
+  document.querySelector(".controls .selected").classList.remove("selected");
   slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
 }
 
-// document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
-//   indicator.addEventListener("click", function () {
-//     sectionIndex = ind;
-//     setIndex();
-//     indicator.classList.add("selected");
-//   });
-// });
+document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
+  indicator.addEventListener("click", function () {
+    sectionIndex = ind;
+    setIndex();
+    indicator.classList.add("selected");
+  });
+});
 
 leftArrow.addEventListener("click", function () {
   sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
-  // indicatorParents.children[sectionIndex].classList.add("selected");
+  indicatorParents.children[sectionIndex].classList.add("selected");
   setIndex();
 });
 
@@ -31,8 +31,7 @@ rightArrow.addEventListener("click", function () {
   } else {
     sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
   }
-
-  // indicatorParents.children[sectionIndex].classList.add("selected");
+  indicatorParents.children[sectionIndex].classList.add("selected");
   setIndex();
 });
 
