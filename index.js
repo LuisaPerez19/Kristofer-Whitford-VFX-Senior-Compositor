77dc465cdc534fe83201a98922e9d07807e02ef8
Luisa Perez <luisa.perezw3@gmail.com>
2023-08-04 16:54:32 +0100

    adding indicators to match loop

--- index.js
+++ index.js
@@ -9,19 +9,18 @@ var sectionIndex = 0;
 function setIndex() {
   document.querySelector(".controls .selected").classList.remove("selected");
   slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
+  indicatorParents.children[sectionIndex].classList.add("selected");
 }
 
 document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
   indicator.addEventListener("click", function () {
     sectionIndex = ind;
     setIndex();
-    indicator.classList.add("selected");
   });
 });
 
 leftArrow.addEventListener("click", function () {
   sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
-  indicatorParents.children[sectionIndex].classList.add("selected");
   setIndex();
 });
 
@@ -31,12 +30,5 @@ rightArrow.addEventListener("click", function () {
   } else {
     sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
   }
-  indicatorParents.children[sectionIndex].classList.add("selected");
   setIndex();
 });
-
-// rightArrow.addEventListener("click", function () {
-//   sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
-//   indicatorParents.children[sectionIndex].classList.add("selected");
-//   setIndex();
-// });
