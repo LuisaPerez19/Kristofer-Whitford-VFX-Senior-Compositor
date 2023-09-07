be7ca4dca8efb6485bcef2d46b98524dee60ef83
Luisa Perez <luisa.perezw3@gmail.com>
2023-09-07 16:09:10 +0100

    adding contact form google drive API and correcting about me info

--- about.js
+++ about.js
@@ -12,3 +12,23 @@ function opentab(event, tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+// contact me----------------------------
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzlCiozWJ7rYNHPoz-vilieQw-ddtHNGBrkVc9Tw_RYg1IUZk7rMWKuPDEdw_5c5YZTWQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+console.log(msg);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
